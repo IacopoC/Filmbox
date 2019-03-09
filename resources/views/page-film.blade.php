38240f6a83abc53dad25ff601cfbb7d0c1b0964c
iacopo <iacopo.cutino@gmail.com>
2019-03-09 13:08:40 +0100

    Inserito accordion cast pagine film, corretto errore js in pagine film, modifiche stile

--- resources/views/page-film.blade.php
+++ resources/views/page-film.blade.php
@@ -64,12 +64,27 @@
                             $result_genre[] = $film_genre->name;
                             endforeach; ?>
 
-                            <h4>Cast</h4>
-                            @foreach($credits_obj->cast as $credits_people)
-                                    <?php $credits_arr[] = $credits_people->character . " (" . $credits_people->name . ")"; ?>
+                         <!--Inizio sezione cast-->
+                             @foreach($credits_obj->cast as $credits_people)
+                                 <?php $credits_arr[] = $credits_people->character . " (" . $credits_people->name . ")"; ?>
 
                              @endforeach
-                               <p>{{ implode(", ",$credits_arr) }}</p>
+                             <div class="card">
+                                 <div class="card-header" id="headingOne">
+                                     <h5 class="mb-0">
+                                         <button class="btn btn-light" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                             <strong>Cast</strong>
+                                         </button>
+                                     </h5>
+                                 </div>
+
+                                 <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                                     <div class="card-body">
+                                         <p>{{ implode(", ",$credits_arr) }}</p>
+                                     </div>
+                                 </div>
+                             </div>
+                         <!--Fine sezione cast-->
 
                           @if(isset($result_genre))
                             <p><strong>Genere:</strong> {{ implode(", ",$result_genre) }} </p>
@@ -93,7 +108,7 @@
                                  @endforeach
 
                              <div class="votes-box">
-                            <p><strong>VOTO MEDIO</strong></p>
+                            <p class="d-inline"><strong>Voto medio: </strong></p>
                                 @if($film_obj->vote_average > 0 and $film_obj->vote_average < 4 )
                                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M15.5,8C16.3,8 17,8.7 17,9.5C17,10.3 16.3,11 15.5,11C14.7,11 14,10.3 14,9.5C14,8.7 14.7,8 15.5,8M10,9.5C10,10.3 9.3,11 8.5,11C7.7,11 7,10.3 7,9.5C7,8.7 7.7,8 8.5,8C9.3,8 10,8.7 10,9.5M12,14C13.75,14 15.29,14.72 16.19,15.81L14.77,17.23C14.32,16.5 13.25,16 12,16C10.75,16 9.68,16.5 9.23,17.23L7.81,15.81C8.71,14.72 10.25,14 12,14Z" /></svg></label>
 
@@ -174,7 +189,7 @@
          </section>
          @endif
          <!--Fine film simili-->
-
+    @if (Auth::check())
          <script>
         
     document.getElementById('rating-form').addEventListener('submit',getRating);
@@ -238,4 +253,5 @@
         })
     });
          </script>
+    @endif
 @endsection
